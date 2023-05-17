e2fb2df9f4c72e403302b21edbb7d61d58471bf5
Mark Brown <mark.brown@unosquare.com>
2023-05-17 18:57:11 +0100

    Added javascript implementation

---
+++ javascript/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { json, urlencoded } = require("body-parser");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(morgan("tiny"));
+app.use(urlencoded({ extended: true }));
+
+const words = ["Banana", "Canine", "Unosquare", "Airport"];
+const games = {};
+
+const retrieveWord = () => words[Math.floor(Math.random(words.length - 1))];
+
+app.post("/games", (req, res) => {
+  const newGameWord = retrieveWord();
+  const newGameId = uuid();
+  const newGame = {
+    remainingGuesses: 6,
+    unmaskedWord: newGameWord,
+    word: newGameWord.replace(/[^a-zA-Z0-9]/, "_"),
+    status: "In Progress",
+    incorrectGuesses: []
+  };
+
+  games[newGameId] = newGame;
+
+  res.send(newGameId);
+});
+
+app.get("/games/:gameId", (req, res) => {
+    const { gameId } = req.params;
+    if (!gameId) return res.sendStatus(404);
+
+    var game = games[gameId];
+    if (!game) {
+        return res.sendStatus(404); 
+    }
+
+    res.status(200).json(game);
+});
+
+app.post("/games/:gameId/guesses", (req, res) => {
+    const { gameId } = req.params;
+    const { letter } = req.body;
+
+    if (!gameId) return res.sendStatus(404);
+
+    var game = games[gameId];
+    if (!game) return res.sendStatus(404); 
+
+    if (!letter || letter.length != 1) {
+        return res.status(400).json({
+            Message: "Guess must be supplied with 1 letter"
+        })
+    }
+
+    return res.status(200).json(game);
+});
+
+app.listen(3000);
